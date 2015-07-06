ac22b6f86fb01adfd5fce491c09562bbac891696
beeva-lauraduato <laura.duato@beeva.com>
2015-07-06 15:11:10 +0200

    adding refresh token

--- app/databases.js
+++ app/databases.js
@@ -14,6 +14,7 @@ var Schema = mongoose.Schema;
 
 var UserTokens = new Schema({
 	access_token		: { type : String },
+	refresh_token		: { type : String },
  	token_type			: { type : String },
   	id_token			: { type : String },
   	expiry_date			: { type : Date }
